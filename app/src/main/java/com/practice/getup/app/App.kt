b251d1ac7598a781ea5b112928efec638dbdea57
alexxk2 <alexx.koda@gmail.com>
2023-06-20 14:17:36 +0300

    20.06.2023
    -созданы ночные стили для всех экранов
    -сделана логика смены на ночной режим
    -добавленые русские и английские стринги
    -добавлена логика смены языка
    
    Tasks:
    
    -баг, при перевороте таймера сбрасывается прогресс
    
    -подумать над тем, чтобы сделать мульти таймеровое приложение, т.е. сделать возможность прекращать тренировку, а также пускать её на заднем фоне с кнопкой континью и минитаймером вместо start workout. Так можно будет вести несколько тренировок, но надо будет делать фабрики вместо синглов в дата слое и что-то придумать со звуками( в целом спорная фича)
    
    -почистить код от мусора(старого кода, лишних картинок, стрингов и тд)
    
    -сделать выбор при нажатии назад(отменить тренировку или нет) поменять кнопку старт на главной с старт на котинью

--- app/src/main/java/com/practice/getup/app/App.kt
+++ app/src/main/java/com/practice/getup/app/App.kt
@@ -1,6 +1,8 @@
 package com.practice.getup.app
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import com.practice.getup.data.db.WorkoutDatabase
 import com.practice.getup.di.dataModule
 import com.practice.getup.di.domainModule
@@ -9,9 +11,17 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
+import java.util.*
+
+const val SHARED_PREFS = "shared_prefs"
+const val IS_DARK_THEME = "is_dark_theme"
+const val LANGUAGE = "language"
 
 class App: Application() {
+
     val workoutDatabase: WorkoutDatabase by lazy { WorkoutDatabase.getDataBase(this) }
+    var darkTheme = false
+    var language: String? = "en"
 
     override fun onCreate() {
         super.onCreate()
@@ -25,5 +35,51 @@ class App: Application() {
                 presentationModule
             ))
         }
+
+        val sharedPrefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE)
+        darkTheme = sharedPrefs.getBoolean(IS_DARK_THEME, false)
+        language = sharedPrefs.getString(LANGUAGE, "en")
+
+
+        setNightMode(darkTheme)
+        choseLanguage(language!!)
     }
+
+    fun switchNightMode(isDarkThemeEnabled: Boolean) {
+        darkTheme = isDarkThemeEnabled
+
+        val sharedPrefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE)
+        sharedPrefs.edit()
+            .putBoolean(IS_DARK_THEME, darkTheme)
+            .apply()
+
+        setNightMode(isDarkThemeEnabled)
+    }
+
+    private fun setNightMode(isDarkThemeEnabled: Boolean) {
+        AppCompatDelegate.setDefaultNightMode(
+            if (isDarkThemeEnabled) AppCompatDelegate.MODE_NIGHT_YES
+            else AppCompatDelegate.MODE_NIGHT_NO
+        )
+    }
+
+    fun switchLanguage(languageChoice: String) {
+        language = languageChoice
+
+        val sharedPrefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE)
+        sharedPrefs.edit()
+            .putString(LANGUAGE, language)
+            .apply()
+
+        choseLanguage(language!!)
+    }
+    private fun choseLanguage(languageChoice: String) {
+        val locale = Locale(languageChoice)
+        val appLocale2 = LocaleListCompat.create(locale)
+        // Call this on the main thread as it may require Activity.restart()
+        AppCompatDelegate.setApplicationLocales(appLocale2)
+    }
+
+
+
 }
--- app/src/main/java/com/practice/getup/presentation/settings/ui/SettingsFragment.kt
+++ app/src/main/java/com/practice/getup/presentation/settings/ui/SettingsFragment.kt
@@ -7,6 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.practice.getup.app.App
+import com.practice.getup.app.IS_DARK_THEME
+import com.practice.getup.app.LANGUAGE
+import com.practice.getup.app.SHARED_PREFS
 import com.practice.getup.databinding.FragmentSettingsBinding
 import com.practice.getup.presentation.settings.view_model.SettingsViewModel
 
@@ -36,10 +40,52 @@ class SettingsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.backButton.setOnClickListener { findNavController().navigateUp() }
+        setDarkThemeSwitcher()
+        setLanguageChip()
+
+
+        with(binding){
+            darkThemeSwitcher.setOnCheckedChangeListener { _, isChecked ->
+                (activity?.applicationContext as App).switchNightMode(isChecked)
+            }
+
+            backButton.setOnClickListener { findNavController().navigateUp() }
+
+            darkThemeSwitcher.setOnCheckedChangeListener { _, isChecked ->
+                (activity?.applicationContext as App).switchNightMode(isChecked)
+            }
+
+            chipEnglish.setOnClickListener {
+                (activity?.applicationContext as App).switchLanguage("en")
+            }
+
+            chipRussian.setOnClickListener {
+                (activity?.applicationContext as App).switchLanguage("ru")
+            }
+        }
 
     }
 
+    private fun setDarkThemeSwitcher(){
+        val sharedPrefs = activity?.getSharedPreferences(SHARED_PREFS,0)
+        val isDarkTheme = sharedPrefs?.getBoolean(IS_DARK_THEME,false)
+        binding.darkThemeSwitcher.isChecked = isDarkTheme!!
+    }
+
+    private fun setLanguageChip(){
+        val sharedPrefs = activity?.getSharedPreferences(SHARED_PREFS,0)
+        val currentLanguage = sharedPrefs?.getString(LANGUAGE,"en")
+        when(currentLanguage){
+            "en" ->{
+                binding.chipRussian.isChecked = false
+                binding.chipEnglish.isChecked = true
+            }
+            else -> {
+                binding.chipRussian.isChecked = true
+                binding.chipEnglish.isChecked = false
+            }
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
